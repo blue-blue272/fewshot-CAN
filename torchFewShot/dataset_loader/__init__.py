a71a40576fd3e2311108c35dc852df4e4dedd8ad
blue-blue272 <56821684+blue-blue272@users.noreply.github.com>
2019-12-18 12:04:28 +0800

    Add files via upload

--- torchFewShot/dataset_loader/__init__.py
+++ torchFewShot/dataset_loader/__init__.py
@@ -1,1 +1,22 @@
+from __future__ import absolute_import
+
+from .train_loader import FewShotDataset_train
+from .test_loader import FewShotDataset_test
+
+
+__loader_factory = {
+        'train_loader': FewShotDataset_train,
+        'test_loader': FewShotDataset_test,
+}
+
+
+
+def get_names():
+    return list(__loader_factory.keys()) 
+
+
+def init_loader(name, *args, **kwargs):
+    if name not in list(__loader_factory.keys()):
+        raise KeyError("Unknown model: {}".format(name))
+    return __loader_factory[name](*args, **kwargs)
 
---
+++ torchFewShot/dataset_loader/test_loader.py
@@ -0,0 +1,145 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import os
+from PIL import Image
+import numpy as np
+import os.path as osp
+import lmdb
+import io
+import random
+
+import torch
+from torch.utils.data import Dataset
+
+
+def read_image(img_path):
+    """Keep reading image until succeed.
+    This can avoid IOError incurred by heavy IO process."""
+    got_img = False
+    if not osp.exists(img_path):
+        raise IOError("{} does not exist".format(img_path))
+    while not got_img:
+        try:
+            img = Image.open(img_path).convert('RGB')
+            got_img = True
+        except IOError:
+            print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+            pass
+    return img
+
+
+class FewShotDataset_test(Dataset):
+    """Few shot epoish Dataset
+
+    Returns a task (Xtrain, Ytrain, Xtest, Ytest) to classify'
+        Xtrain: [nKnovel*nExpemplars, c, h, w].
+        Ytrain: [nKnovel*nExpemplars].
+        Xtest:  [nTestNovel, c, h, w].
+        Ytest:  [nTestNovel].
+    """
+
+    def __init__(self,
+                 dataset, # dataset of [(img_path, cats), ...].
+                 labels2inds, # labels of index {(cats: index1, index2, ...)}.
+                 labelIds, # train labels [0, 1, 2, 3, ...,].
+                 nKnovel=5, # number of novel categories.
+                 nExemplars=1, # number of training examples per novel category.
+                 nTestNovel=2*5, # number of test examples for all the novel categories.
+                 epoch_size=2000, # number of tasks per eooch.
+                 transform=None,
+                 load=True,
+                 **kwargs
+                 ):
+        
+        self.dataset = dataset
+        self.labels2inds = labels2inds
+        self.labelIds = labelIds
+        self.nKnovel = nKnovel
+        self.transform = transform
+
+        self.nExemplars = nExemplars
+        self.nTestNovel = nTestNovel
+        self.epoch_size = epoch_size
+        self.load = load
+
+        seed = 112
+        random.seed(seed)
+        np.random.seed(seed)
+
+        self.Epoch_Exemplar = []
+        self.Epoch_Tnovel = []
+        for i in range(epoch_size):
+            Tnovel, Exemplar = self._sample_episode()
+            self.Epoch_Exemplar.append(Exemplar)
+            self.Epoch_Tnovel.append(Tnovel)
+
+    def __len__(self):
+        return self.epoch_size
+
+    def _sample_episode(self):
+        """sampels a training epoish indexs.
+        Returns:
+            Tnovel: a list of length 'nTestNovel' with 2-element tuples. (sample_index, label)
+            Exemplars: a list of length 'nKnovel * nExemplars' with 2-element tuples. (sample_index, label)
+        """
+
+        Knovel = random.sample(self.labelIds, self.nKnovel)
+        nKnovel = len(Knovel)
+        assert((self.nTestNovel % nKnovel) == 0)
+        nEvalExamplesPerClass = int(self.nTestNovel / nKnovel)
+
+        Tnovel = []
+        Exemplars = []
+        for Knovel_idx in range(len(Knovel)):
+            ids = (nEvalExamplesPerClass + self.nExemplars)
+            img_ids = random.sample(self.labels2inds[Knovel[Knovel_idx]], ids) 
+
+            imgs_tnovel = img_ids[:nEvalExamplesPerClass]
+            imgs_emeplars = img_ids[nEvalExamplesPerClass:]
+
+            Tnovel += [(img_id, Knovel_idx) for img_id in imgs_tnovel]
+            Exemplars += [(img_id, Knovel_idx) for img_id in imgs_emeplars]
+        assert(len(Tnovel) == self.nTestNovel)
+        assert(len(Exemplars) == nKnovel * self.nExemplars)
+        random.shuffle(Exemplars)
+        random.shuffle(Tnovel)
+
+        return Tnovel, Exemplars
+
+    def _creatExamplesTensorData(self, examples):
+        """
+        Creats the examples image label tensor data.
+
+        Args:
+            examples: a list of 2-element tuples. (sample_index, label).
+
+        Returns:
+            images: a tensor [nExemplars, c, h, w]
+            labels: a tensor [nExemplars]
+        """
+
+        images = []
+        labels = []
+        for (img_idx, label) in examples:
+            img = self.dataset[img_idx][0]
+            if self.load:
+                img = Image.fromarray(img)
+            else:
+                img = read_image(img)
+            if self.transform is not None:
+                img = self.transform(img)
+            images.append(img)
+            labels.append(label)
+        images = torch.stack(images, dim=0)
+        labels = torch.LongTensor(labels)
+        return images, labels
+
+    def __getitem__(self, index):
+        Tnovel = self.Epoch_Tnovel[index]
+        Exemplars = self.Epoch_Exemplar[index]
+        Xt, Yt = self._creatExamplesTensorData(Exemplars)
+        Xe, Ye = self._creatExamplesTensorData(Tnovel)
+        return Xt, Yt, Xe, Ye
+
