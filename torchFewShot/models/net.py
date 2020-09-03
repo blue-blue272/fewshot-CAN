6055985e82944c305f0fbf4d1b3e0eef1833d06c
blue-blue272 <56821684+blue-blue272@users.noreply.github.com>
2020-09-03 15:38:43 +0800

    Add files via upload

--- torchFewShot/models/net.py
+++ torchFewShot/models/net.py
@@ -7,10 +7,20 @@ from resnet12 import resnet12
 from cam import CAM
 
 
+def one_hot(labels_train):
+    labels_train = labels_train.cpu()
+    nKnovel = 5
+    labels_train_1hot_size = list(labels_train.size()) + [nKnovel,]
+    labels_train_unsqueeze = labels_train.unsqueeze(dim=labels_train.dim())
+    labels_train_1hot = torch.zeros(labels_train_1hot_size).scatter_(len(labels_train_1hot_size) - 1, labels_train_unsqueeze, 1)
+    return labels_train_1hot
+
+
 class Model(nn.Module):
-    def __init__(self, scale_cls, num_classes=64):
+    def __init__(self, scale_cls, iter_num_prob=35.0/75, num_classes=64):
         super(Model, self).__init__()
         self.scale_cls = scale_cls
+        self.iter_num_prob = iter_num_prob
 
         self.base = resnet12()
         self.cam = CAM()
@@ -64,5 +74,72 @@ class Model(nn.Module):
         ftest = torch.matmul(ftest, ytest) 
         ftest = ftest.view(batch_size * num_test, -1, 6, 6)
         ytest = self.clasifier(ftest)
-
         return ytest, cls_scores
+
+    def helper(self, ftrain, ftest, ytrain):
+        b, n, c, h, w = ftrain.size()
+        k = ytrain.size(2)
+
+        ytrain_transposed = ytrain.transpose(1, 2)
+        ftrain = torch.bmm(ytrain_transposed, ftrain.view(b, n, -1))
+        ftrain = ftrain.div(ytrain_transposed.sum(dim=2, keepdim=True).expand_as(ftrain))
+        ftrain = ftrain.view(b, -1, c, h, w)
+
+        ftrain, ftest = self.cam(ftrain, ftest)
+        ftrain = ftrain.mean(-1).mean(-1)
+        ftest = ftest.mean(-1).mean(-1)
+
+        ftest = F.normalize(ftest, p=2, dim=ftest.dim()-1, eps=1e-12)
+        ftrain = F.normalize(ftrain, p=2, dim=ftrain.dim()-1, eps=1e-12)
+        scores = self.scale_cls * torch.sum(ftest * ftrain, dim=-1)
+        return scores
+
+    def test_transductive(self, xtrain, xtest, ytrain, ytest):
+        iter_num_prob = self.iter_num_prob
+        batch_size, num_train = xtrain.size(0), xtrain.size(1)
+        num_test = xtest.size(1)
+        K = ytrain.size(2)
+
+        xtrain = xtrain.view(-1, xtrain.size(2), xtrain.size(3), xtrain.size(4))
+        xtest = xtest.view(-1, xtest.size(2), xtest.size(3), xtest.size(4))
+        x = torch.cat((xtrain, xtest), 0)
+        f = self.base(x)
+
+        ftrain = f[: batch_size*num_train].view(batch_size, num_train, *f.size()[1:])
+        ftest = f[batch_size*num_train:].view(batch_size, num_test, *f.size()[1:])
+        cls_scores = self.helper(ftrain, ftest, ytrain)
+
+        num_images_per_iter = int(num_test * iter_num_prob)
+        num_iter = num_test // num_images_per_iter
+
+        for i in range(num_iter):
+            max_scores, preds = torch.max(cls_scores, 2)
+            chose_index = torch.argsort(max_scores.view(-1), descending=True)
+            chose_index = chose_index[: num_images_per_iter * (i + 1)]
+
+            ftest_iter = ftest[0, chose_index].unsqueeze(0)
+            preds_iter = preds[0, chose_index].unsqueeze(0)
+            preds_iter = one_hot(preds_iter).cuda()
+
+            ftrain_iter = torch.cat((ftrain, ftest_iter), 1)
+            ytrain_iter = torch.cat((ytrain, preds_iter), 1)
+            cls_scores = self.helper(ftrain_iter, ftest, ytrain_iter)
+
+        return cls_scores
+
+
+
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+
+    net = Model(scale_cls=7)
+    net.eval()
+
+    x1 = torch.rand(1, 5, 3, 84, 84)
+    x2 = torch.rand(1, 75, 3, 84, 84)
+    y1 = torch.rand(1, 5, 5)
+    y2 = torch.rand(1, 75, 5)
+
+    y1 = net.test_transductive(x1, x2, y1, y2)
+    print(y1.size())
